2fdbef44028dc66e7590370421f6f61f5d50dc4a
Diego <diego@diegosilva.com.br>
2019-02-24 22:29:58 -0300

    fixes

--- kvstore/src/main/scala/kvstore/Replicator.scala
+++ kvstore/src/main/scala/kvstore/Replicator.scala
@@ -3,6 +3,7 @@ package kvstore
 import akka.actor.Props
 import akka.actor.Actor
 import akka.actor.ActorRef
+import akka.event._
 import scala.concurrent.duration._
 
 object Replicator {
@@ -30,16 +31,38 @@ class Replicator(val replica: ActorRef) extends Actor {
   var pending = Vector.empty[Snapshot]
   
   var _seqCounter = 0L
-  def nextSeq() = {
+  def nextSeq = {
     val ret = _seqCounter
     _seqCounter += 1
     ret
   }
 
-  
+  val log = Logging(context.system, this)
+
+  context.system.scheduler.schedule(0.milliseconds, 100.milliseconds) {
+    acks foreach { case (seq, (_, Replicate(k, v, id))) => {
+        replica ! Snapshot(k, v, seq)
+      }
+    }
+  }
+
   /* TODO Behavior for the Replicator. */
-  def receive: Receive = {
-    case _ =>
+  def receive: Receive = LoggingReceive {
+    case Replicate(key, valueOption, id) => {
+      val seq = nextSeq
+      acks += seq -> (sender, Replicate(key, valueOption, id))
+      replica ! Snapshot(key, valueOption, seq)
+    }
+    case SnapshotAck(key, seq) => {
+      val ack = acks.get(seq)
+      ack match {
+        case Some((primary, Replicate(k, v, id))) => {
+          acks -= seq
+          primary ! Replicated(k, id)
+        }
+        case None => 
+      }
+    }
   }
 
 }
